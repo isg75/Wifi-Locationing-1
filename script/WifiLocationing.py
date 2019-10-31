9876128c4b2f087760ef5d54fc4962265c6dd672
alinebarbosa <liny_barbosa@hotmail.com>
2019-10-31 16:23:27 +0100

    predicting latitude per building and longitude per floor

--- script/WifiLocationing.py
+++ script/WifiLocationing.py
@@ -29,7 +29,6 @@ from sklearn.svm import SVC
 training_data_path = '/home/aline/Documentos/Ubiqum/IoT Analytics/WifiLocationing/data/trainingData.csv'
 validation_data_path = '/home/aline/Documentos/Ubiqum/IoT Analytics/WifiLocationing/data/validationData.csv'
 
-
 # read the data and store data in DataFrame
 training_data = pd.read_csv(training_data_path) 
 validation_data = pd.read_csv(validation_data_path)
@@ -48,7 +47,7 @@ training_data.isnull().any().sum()
 validation_data.isnull().any().sum()
 
 """Pre processing"""
-# Feature selection
+# Feature selection - Pick only WAPs
 training = training_data.iloc[:,0:520]
 validation = validation_data.iloc[:,0:520]
 
@@ -62,14 +61,12 @@ building = training_data.BUILDINGID
 floor = pd.Series(floor, dtype="category")
 building = pd.Series(building, dtype="category")
 
-"""Cross-validation"""
+# Cross-validation
 def get_score_rfr(n_estimators):
     my_pipeline = Pipeline(steps=[
-#        ('preprocessor', SimpleImputer()),
         ('model', RandomForestRegressor(n_estimators=n_estimators, 
                                         random_state=0))
     ])
-#    scores = []
     scores = -1 * cross_val_score(my_pipeline, training, latitude,
                                       cv=3,
                                       scoring='neg_mean_absolute_error')
@@ -83,35 +80,6 @@ for i in number_of_trees:
     results.update({i:get_score_rfr(i)})
 
 """Normalize"""
-# Transform data - Put 'not found' signals (100) as the min
-for i in range(0,len(training.columns)):
-    if (training.iloc[:,[i]].min() == 100).bool():
-        training.iloc[:,[i]] = (training.min()).min()
-    else:
-        training.iloc[:,[i]] = \
-        training.iloc[:,[i]].replace(100, training.iloc[:,[i]].min())
-
-training.values
-
-# Do the same with the validation data
-for i in range(0,len(validation.columns)):
-    if (validation.iloc[:,[i]].min() == 100).bool():
-        validation.iloc[:,[i]] = (validation.min()).min()
-    else:
-        validation.iloc[:,[i]] = \
-        validation.iloc[:,[i]].replace(100, validation.iloc[:,[i]].min())
-
-validation.values
-
-
-for i in range(0,len(training.columns)):
-    if (training.iloc[:,[i]].min()==training.iloc[:,[i]].max()).bool():
-        training.iloc[:,[i]] = 0
-    else:
-        training.iloc[:,[i]] = \
-        (training.iloc[:,[i]] - training.iloc[:,[i]].min())/ \
-        (training.iloc[:,[i]].max() - training.iloc[:,[i]].min())
-
 # Create function to transform putting 'not found' signals (100) as 
 # the min and normalize data
 def normalize(df):
@@ -136,21 +104,6 @@ def normalize(df):
 # Apply function on training and validation data
 training_normalized = normalize(training)
 validation_normalized = normalize(validation)
-
-"""Plots"""
-scatter_training = px.scatter_3d(training_data, 
-                    x="LONGITUDE", 
-                    y="LATITUDE", 
-                    z="FLOOR", 
-                    color="BUILDINGID")
-plot(scatter_training)
-
-scatter_validation = px.scatter_3d(validation_data, 
-                    x="LONGITUDE", 
-                    y="LATITUDE", 
-                    z="FLOOR", 
-                    color="BUILDINGID")
-plot(scatter_validation)
 
 """Standardize"""
 #Robust Scaler
@@ -274,27 +227,51 @@ knn_model.fit(training_building,floor)
 knn_predictions_floor = knn_model.predict(validation_building)
 accuracy_score(validation_data.FLOOR, knn_predictions_floor)
 
-""""""
-t = training_floor.iloc[:,0:520]
-t[t < 0] = 0
-t['BUILDINGID'] = building
-t['FLOOR'] = floor 
-
-v = validation_floor.iloc[:,0:520]
-v[v < 0] = 0
-v['BUILDINGID'] = svc_predictions
-v['FLOOR'] = svc_predictions_floor 
-""""""
 #Random Forest Regressor - Latitude
-# Create new columns with the building in both data set
-validation_floor = validation_building
+# Select WAPs >=0 and create new columns with the floor in both data set
+validation_floor = validation_building.iloc[:,0:520]
+validation_floor[validation_floor < 0] = 0
+validation_floor['BUILDINGID'] = svc_predictions
 validation_floor['FLOOR'] = svc_predictions_floor 
-training_floor = training_building
+training_floor = training_building.iloc[:,0:520]
+training_floor[training_floor < 0] = 0
+training_floor['BUILDINGID'] = building
 training_floor['FLOOR'] = floor 
-# Fit the model
+
+# Find index and slipting data per building
+index_b0 = building.index[building == 0]
+index_b1 = building.index[building == 1]
+index_b2 = building.index[building == 2]
+training_floor_b0 = training_floor.iloc[index_b0,0:522]
+training_floor_b1 = training_floor.iloc[index_b1,0:522]
+training_floor_b2 = training_floor.iloc[index_b2,0:522]
+latitude_b0 = latitude.iloc[index_b0]
+latitude_b1 = latitude.iloc[index_b1]
+latitude_b2 = latitude.iloc[index_b2]
+index_b0_val = validation_floor.index[validation_floor['BUILDINGID'] == 0]
+index_b1_val = validation_floor.index[validation_floor['BUILDINGID'] == 1]
+index_b2_val = validation_floor.index[validation_floor['BUILDINGID'] == 2]
+validation_floor_b0 = validation_floor.iloc[index_b0_val,0:522]
+validation_floor_b1 = validation_floor.iloc[index_b1_val,0:522]
+validation_floor_b2 = validation_floor.iloc[index_b2_val,0:522]
+
+# Fit the model per building
 rf_model = RandomForestRegressor(random_state = 2)
-rf_model.fit(t,latitude)
-rf_predictions_latitude = rf_model.predict(v)
+rf_model.fit(training_floor_b0,latitude_b0)
+rf_predictions_latitude_b0 = rf_model.predict(validation_floor_b0)
+rf_model.fit(training_floor_b1,latitude_b1)
+rf_predictions_latitude_b1 = rf_model.predict(validation_floor_b1)
+rf_model.fit(training_floor_b2,latitude_b2)
+rf_predictions_latitude_b2 = rf_model.predict(validation_floor_b2)
+pred_latitude = np.empty(len(validation_data))
+pred_latitude[index_b0_val] = rf_predictions_latitude_b0
+pred_latitude[index_b1_val] = rf_predictions_latitude_b1
+pred_latitude[index_b2_val] = rf_predictions_latitude_b2
+mean_absolute_error(validation_data.LATITUDE, pred_latitude)
+
+# Fit the model - general
+rf_model.fit(training_floor,latitude)
+rf_predictions_latitude = rf_model.predict(validation_floor)
 mean_absolute_error(validation_data.LATITUDE, rf_predictions_latitude)
 
 #XGBoost - Latitude
@@ -307,15 +284,64 @@ xgb_predictions_latitude = xgb_model.predict(validation_floor)
 mean_absolute_error(validation_data.LATITUDE, xgb_predictions_latitude)
 
 #Random Forest Regressor - Longitude
-# Create new columns with the building in both data set
+# Create new columns with the latitude in both data set
 validation_latitude = validation_floor
 validation_latitude['LATITUDE'] = rf_predictions_latitude 
 training_latitude = training_floor
-training_latitude['LATITUDE'] = latitude 
-# Fit the model
+training_latitude['LATITUDE'] = latitude
+# Fit the model - General
 rf_model.fit(training_latitude, longitude)
 rf_predictions_longitude = rf_model.predict(validation_latitude)
-mean_absolute_error(validation_data.LONGITUDE, rf_predictions_longitude)
+mean_absolute_error(validation_data.LONGITUDE, rf_predictions_longitude) 
+
+# Find index and slipting data per floor
+index_f0 = floor.index[floor == 0]
+index_f1 = floor.index[floor == 1]
+index_f2 = floor.index[floor == 2]
+index_f3 = floor.index[floor == 3]
+index_f4 = floor.index[floor == 4]
+training_latitude_f0 = training_latitude.iloc[index_f0,0:523]
+training_latitude_f1 = training_latitude.iloc[index_f1,0:523]
+training_latitude_f2 = training_latitude.iloc[index_f2,0:523]
+training_latitude_f3 = training_latitude.iloc[index_f3,0:523]
+training_latitude_f4 = training_latitude.iloc[index_f4,0:523]
+longitude_f0 = longitude.iloc[index_f0]
+longitude_f1 = longitude.iloc[index_f1]
+longitude_f2 = longitude.iloc[index_f2]
+longitude_f3 = longitude.iloc[index_f3]
+longitude_f4 = longitude.iloc[index_f4]
+
+index_f0_val = validation_floor.index[validation_floor['FLOOR'] == 0]
+index_f1_val = validation_floor.index[validation_floor['FLOOR'] == 1]
+index_f2_val = validation_floor.index[validation_floor['FLOOR'] == 2]
+index_f3_val = validation_floor.index[validation_floor['FLOOR'] == 3]
+index_f4_val = validation_floor.index[validation_floor['FLOOR'] == 4]
+validation_latitude_floor = validation_floor.iloc[:,0:522]
+validation_latitude_floor['LATITUDE'] = pred_latitude
+validation_latitude_f0 = validation_latitude_floor.iloc[index_f0_val,:]
+validation_latitude_f1 = validation_latitude_floor.iloc[index_f1_val,:]
+validation_latitude_f2 = validation_latitude_floor.iloc[index_f2_val,:]
+validation_latitude_f3 = validation_latitude_floor.iloc[index_f3_val,:]
+validation_latitude_f4 = validation_latitude_floor.iloc[index_f4_val,:]
+
+# Fit the model per floor
+rf_model.fit(training_latitude_f0,longitude_f0)
+rf_predictions_longitude_f0 = rf_model.predict(validation_latitude_f0)
+rf_model.fit(training_latitude_f1,longitude_f1)
+rf_predictions_longitude_f1 = rf_model.predict(validation_latitude_f1)
+rf_model.fit(training_latitude_f2,longitude_f2)
+rf_predictions_longitude_f2 = rf_model.predict(validation_latitude_f2)
+rf_model.fit(training_latitude_f3,longitude_f3)
+rf_predictions_longitude_f3 = rf_model.predict(validation_latitude_f3)
+rf_model.fit(training_latitude_f4,longitude_f4)
+rf_predictions_longitude_f4 = rf_model.predict(validation_latitude_f4)
+pred_longitude_floor = np.empty(len(validation_data))
+pred_longitude_floor[index_f0_val] = rf_predictions_longitude_f0
+pred_longitude_floor[index_f1_val] = rf_predictions_longitude_f1
+pred_longitude_floor[index_f2_val] = rf_predictions_longitude_f2
+pred_longitude_floor[index_f3_val] = rf_predictions_longitude_f3
+pred_longitude_floor[index_f4_val] = rf_predictions_longitude_f4
+mean_absolute_error(validation_data.LONGITUDE, pred_longitude_floor)
 
 #XGBoost - Longitude
 xgb_model.fit(training_latitude, longitude, 
@@ -325,6 +351,34 @@ xgb_model.fit(training_latitude, longitude,
 xgb_predictions_longitude = xgb_model.predict(validation_latitude)
 mean_absolute_error(validation_data.LONGITUDE, xgb_predictions_longitude)
 
+"""Plots"""
+# Plot training
+scatter_training = px.scatter_3d(training_data, 
+                    x="LONGITUDE", 
+                    y="LATITUDE", 
+                    z="FLOOR", 
+                    color="BUILDINGID")
+plot(scatter_training)
+
+# Plot validation
+scatter_validation = px.scatter_3d(validation_data, 
+                    x="LONGITUDE", 
+                    y="LATITUDE", 
+                    z="FLOOR", 
+                    color="BUILDINGID")
+plot(scatter_validation)
+
+# Create final data with predictions
+validation_predicted = validation_latitude
+validation_predicted['LONGITUDE'] = rf_predictions_longitude
+# Plot results
+scatter_results = px.scatter_3d(validation_predicted, 
+                    x="LONGITUDE", 
+                    y="LATITUDE", 
+                    z="FLOOR", 
+                    color="BUILDINGID")
+plot(scatter_results)
+
 # Try to know H2O to parallelize the processing
 # Try to learn about PCA
 # pipeline - ok
@@ -333,7 +387,7 @@ mean_absolute_error(validation_data.LONGITUDE, xgb_predictions_longitude)
 # hyper parameters and random_state
 # normalize WAPs (remove 100) - ok
 # standarlize WAPs (remove 100) per row - ok
-# pick the high values for each row
+# pick the high values for each row - ok
 # Use floor and building as categorical then use RandomForestClassifier - ok
 # data-leakage
 # plot errors
